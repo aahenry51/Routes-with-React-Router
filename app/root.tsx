6304a5b2d1880a260e5c63546c34b0b0ad29c4db
Alphi <aahenry51@gmail.com>
2025-07-02 11:03:34 -0400

    Nav

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import Navigation from "./nav/navigation"
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -42,7 +43,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+ <Navigation/>
+  <Outlet />
+  </>);
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
